a4b65b55dfc1fdf94b61660d721cb2fbcdceb972
The Unnamed <gobles9638@outlook.com>
2025-02-22 11:46:34 -0500

    Update Constants.java

--- RealThingCommand/RealThingCommand/src/main/java/frc/robot/Constants.java
+++ RealThingCommand/RealThingCommand/src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ package frc.robot;
 public final class Constants {
   public static final class DriveConstants {
     public static final int LEFT_LEADER_ID = 8;
-    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int LEFT_FOLLOWER_ID = 2; 
     public static final int RIGHT_LEADER_ID = 3;
     public static final int RIGHT_FOLLOWER_ID = 4;
 
